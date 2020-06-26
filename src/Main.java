95c8bf32a6479f2746ff0a44ea446c3323f9f63b
Nastya <realovka@mail.ru>
2020-06-26 08:33:38 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
